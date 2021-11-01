5870f2b4a5ddf759e38518af345dc7bdf97a38a0
anandureghu <anandukr145@gmail.com>
2021-11-01 14:14:44 +0530

    Job Model added

--- controller/jobs.js
+++ controller/jobs.js
@@ -1,14 +1,31 @@
+const Job = require('../models/job');
+
 const getAllJobs = async (req, res) => {
-    res.send("Get all jobs")
+    const jobs = await Job.find({createdBy: req.user.userId});
+    res.status(200).json({success: true, nbHits: jobs.length, jobs});
 }
 
 const getJob = async(req, res) => {
     const {id} = req.params;
-    res.send(`Get a single job with id: ${id}`);
+    try {
+        
+        const job = await Job.findOne({createdBy: req.user.userId, _id: id});
+    } catch (error) {
+        return res.status(404).json({success:false, msg: `No job found with id ${id}`})
+        
+    }
+    
+    if(!job){
+        return res.status(404).json({success:false, msg: `No job found with id ${id}`})
+    }
+
+    res.status(200).json({success:false, job});
 }
 
 const createJob = async(req, res) => {
-    res.send(`Create a job`);
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body);
+    res.status(201).json({success: true, job});
 }
 
 const updateJob = async(req, res) => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,22 @@
 const jwt = require('jsonwebtoken');
 
-const authenticationMiddleware = (req, res) => {
+const authenticationMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if(!authHeader || !authHeader.startsWith("Bearer ")){
         return res.status(401).json({sucess: false, "msg": "Unauthorized"});
     }
     const token = authHeader.split(" ")[1];
-    const user = jwt.verify(token, process.env.JWT_SECRET);
+
+    if(token){
+        try {
+            const payload = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = {userId: payload.userId, name: payload.username};
+            next();
+        } catch (error) {
+            return res.status(401).json({sucess: false, "msg": "Unauthorized"});
+        }
+    }
+
 }
 
 module.exports = authenticationMiddleware;
